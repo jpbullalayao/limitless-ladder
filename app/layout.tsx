c4af94c88625dfba3c3260da4a96ec1a2b3a1fe2
Jourdan Bul-lalayao <jpbullalayao@gmail.com>
2025-01-26 21:29:02 -0800

    Update meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import Header from '@/components/Header'
 
 export const metadata = {
-  title: 'VGC Ladder',
+  title: 'VGC Leaderboard',
   description: 'ELO Leaderboard for VGC tournaments on Limitless',
 }
 
